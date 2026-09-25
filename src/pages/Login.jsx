a445ce7740a7d2fa2abf13d8c4e2dfa85197092d
stoychodebrenliev <stoichodebrenliev@gmail.com>
2026-09-25 16:17:14 +0200

    Create Login and Register Flow

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,45 @@
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
+import { useState } from "react";
+import { supabase } from "../lib/supabaseClient";
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const navigate = useNavigate();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+
+        setErrorMessage("");
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        });
+
+        if(error) {
+            setErrorMessage(error.message);
+            return;
+        }
+
+        navigate("/");
+    }
+
     return (
         <main className="auth-page">
             <section className="auth-section">
                 <h1>Login to Your Account</h1>
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <label htmlFor="email">Email</label>
                     <input
                         type="email"
                         id="email"
                         name="email"
                         placeholder="stoycho@gmail.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         required
                     />
 
@@ -22,11 +49,14 @@ export default function Login() {
                         id="password"
                         name="password"
                         placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         required
                     />
 
                     <button type="submit">Login</button>
 
+                    {errorMessage && <p>{errorMessage}</p>}
                     <p>
                         Don't have an account? <Link to="/register">Register here</Link>
                     </p>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,56 @@
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
+import { useState } from "react";
+import { supabase } from "../lib/supabaseClient";
 
 export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [successMessage, setSuccessMessage] = useState("");
+
+    const navigate = useNavigate();
+
+    async function handleRegister(e) {
+        e.preventDefault();
+
+        setErrorMessage("");
+        setSuccessMessage("");
+
+        if(password !== confirmPassword) {
+            setErrorMessage("Passwords do not match!");
+            return;
+        }
+
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+
+        if(error) {
+            setErrorMessage(error.message);
+            return;
+        }
+
+        setSuccessMessage("Registration successful!");
+        navigate("/");
+    }
+
     return (
         <main className="auth-page">
             <section className="auth-section">
                 <h1>Create an Account</h1>
                 <p>Please fill out the form below to create an account.</p>
 
-                <form>
+                <form onSubmit={handleRegister}>
                     <label htmlFor="email">Email</label>
                     <input
                         type="email"
                         id="email"
                         name="email"
                         placeholder="stoycho@gmail.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         required
                     />
 
@@ -23,6 +60,8 @@ export default function Register() {
                         id="password"
                         name="password"
                         placeholder="Create a password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         required
                     />
 
@@ -32,11 +71,15 @@ export default function Register() {
                         id="confirmPassword"
                         name="confirmPassword"
                         placeholder="Confirm your password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
                         required
                     />
 
                     <button type="submit">Register</button>
 
+                    {errorMessage && <p>{errorMessage}</p>}
+                    {successMessage && <p>{successMessage}</p>}
                     <p>
                         Already have an account? <Link to="/login">Login here</Link>
                     </p>
